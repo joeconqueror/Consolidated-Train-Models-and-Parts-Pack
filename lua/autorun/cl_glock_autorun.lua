4a7c66b2a17a6e2f3fccd6148a85f410c72e28d0
SnowyPR <70219295+SnowyPR@users.noreply.github.com>
2026-09-07 01:15:34 -0300

    Add files via upload

--- lua/autorun/cl_glock_autorun.lua
+++ lua/autorun/cl_glock_autorun.lua
@@ -1,1 +1,7 @@
 list.Set("RLC_Truck","models/glock/props/drgw_3ft_archbar.mdl", {diameter = 26,passenger = false,fwd = "-X",ldlmt = 80})
+
+timer.Simple(0, function()
+    if not RLC then return end -- the addon simply doesnt exist.
+
+    RLC.AddTruck("models/snowy/props/vfrgs_truck_hyperslider.mdl")
+end)
