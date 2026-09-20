33b78e683f81a5fc86ea69fda03d8ae313bc2d10
SnowyPR <70219295+SnowyPR@users.noreply.github.com>
2026-09-20 02:41:34 -0300

    Merged autorun files

---
+++ lua/autorun/cl_consolidated_autorun.lua
@@ -0,0 +1,3 @@
+list.Set("RLC_Truck","models/snowy/props/vfrgs_truck_hyperslider.mdl", {diameter = 34,passenger = false,fwd = "-X",ldlmt = 80})
+
+list.Set("RLC_Truck","models/glock/props/drgw_3ft_archbar.mdl", {diameter = 26,passenger = false,fwd = "-X",ldlmt = 80})
